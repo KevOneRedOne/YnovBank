31e3a64d05a2cdebab6c47ef2b6124d326e4f2c1
DamienDrozd <damien.drozd@icloud.com>
2025-06-13 16:53:20 +0200

    fix(api): add swagger

--- api/main.go
+++ api/main.go
@@ -1,15 +1,29 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	_ "api/docs" // <-- important !   (le _ permet d’initialiser le package)
+	"api/models"
+	"api/routes"
+	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	"github.com/swaggo/gin-swagger"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
 
 func main() {
-    r := gin.Default()
+	// Initialisation de la base de données
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+	db.AutoMigrate(&models.User{})
 
-    r.GET("/ping", func(c *gin.Context) {
-        c.JSON(200, gin.H{
-            "message": "pong",
-        })
-    })
+	r := gin.Default()
 
-    r.Run() // écoute sur :8080 par défaut
+	// Ajoute la route Swagger UI
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+	routes.SetupRoutes(r, db)
+	r.Run(":8080")
 }
