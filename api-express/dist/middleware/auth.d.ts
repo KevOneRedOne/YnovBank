fbaf2ae893f03c2b4ebde40e81fb9182fa6a2d37
dems <damien.drozd@icloud.com>
2025-06-25 19:56:02 +0200

    fix(tests): remove dist folder

--- api-express/dist/middleware/auth.d.ts
+++
@@ -1,5 +0,0 @@
-import { Response, NextFunction } from 'express';
-import { IAuthenticatedRequest } from '../types';
-declare const authMiddleware: (req: IAuthenticatedRequest, res: Response, next: NextFunction) => void;
-export default authMiddleware;
-//# sourceMappingURL=auth.d.ts.map
--- api-express/dist/middleware/validation.d.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-export declare const validateRegistration: (req: Request, res: Response, next: NextFunction) => void;
-export declare const validateLogin: (req: Request, res: Response, next: NextFunction) => void;
-export declare const validateAccount: (req: Request, res: Response, next: NextFunction) => void;
-export declare const validateTransaction: (req: Request, res: Response, next: NextFunction) => void;
-//# sourceMappingURL=validation.d.ts.map
