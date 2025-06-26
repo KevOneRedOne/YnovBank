6f055dee9928dc4a15568dfd9b6ee02f0c4b9a9d
clement-lnr <clementlanneretonne@yahoo.fr>
2025-06-26 09:38:18 +0200

    feat: i18n

---
+++ app/layouts/default.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+    import { useRoute } from 'vue-router'
+    import Header from '@/components/AppHeader.vue'
+    
+    const route = useRoute()
+    
+    const isAuthPage = computed(() =>
+      ['login', 'register'].includes(route.name?.toString() || '')
+    )
+</script>
+
+<template>
+  <div>
+    <Header v-if="!isAuthPage" />
+    <slot />
+  </div>
+</template>
